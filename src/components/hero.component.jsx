f7a2d75ecd566c35e76cf419594b72395e631f87
Tran Ho <tranho@Mac-mini.local>
2023-07-10 12:38:37 -0700

    added search bar

--- src/components/hero.component.jsx
+++ src/components/hero.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import backgroundImage from "../assets/norway.jpg"
+import Searchbox from './searchbox.component'
 export default function Hero() {
   return (
-    <div>
-        <div className='image-container bg-[url(../assets/norway.jpg)]'> Search bar</div>
+    <div >
+        <div className='  image-container bg-[url(./assets/norway.jpg)]  bg-origin-border bg-center  bg-cover h-80 flex flex-col justify-center space-y-4'>
+            <div className='text text-white backdrop-blur-sm  text-center p-4'>
+                    <h1 className=' font-bold text-4xl'>Discover new destinations</h1>
+                    <h2 className='font-semibold text-xl'>Wayfarer makes it simple for even the most seasoned travelers to find new journeys to add to their bucket lists.</h2>
+            </div> 
+            <Searchbox/>
+            
+        </div>
+        
         
     </div>
   )
---
+++ src/components/searchbox.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Searchbox() {
+  return (
+    <form>
+        <div className='relative'>
+        
+        <input type="text" placeholder='Enter your destination'className='w-2/4 p-2 pl-5 rounded-lg focus:outline-none' ></input>
+        <button type="submit" className=' bg-blue-700 text-white px-4 py-2  rounded-lg font-medium ml-2'>Search</button>
+        </div>
+        
+    </form>
+  )
+}
